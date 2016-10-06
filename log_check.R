d7be0418f5def5a134ab39b9f030122b209f5dca
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-10-06 09:21:40 -0400

    add log_check to git ignore

---
+++ log_check.R
@@ -0,0 +1,12 @@
+library(cranlogs)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+qm_downld <- cran_downloads("quickmapr",from = "2015-11-03", to = "last-day")
+qm_monthly <- qm_downld %>%
+  mutate(month = month(date),
+         year = year(date))%>%
+  group_by(year,month) %>%
+  summarize(monthly_download = sum(count))
+ggplot(qm_downld,aes(x=date,y=count))+
+  geom_line()+geom_smooth()
